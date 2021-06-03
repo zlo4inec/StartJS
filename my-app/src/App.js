1e0256d9ac022cfd9658173c2849cca46eabe24f
zlo4inec <yasha_synyak@mail.ru>
2021-06-03 19:13:17 +0300

    test
    test

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App= ()=> {
+  return (
+    <div className="App">
+      <div id="square"></div>
+      <li>
+        css
+      </li>
+      <li>
+        html
+      </li>
+      <li>
+        js
+      </li>
+      <li>
+        react
+      </li>
+    </div>
+    
+  );
+}
+
+export default App;
